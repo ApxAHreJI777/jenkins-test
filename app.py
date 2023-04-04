87d9428a581b5762016f98790f594322bd1db158
Sergey Soltan <srg.soltan@gmail.com>
2023-04-04 19:07:15 +0200

    docker compose

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import os
 from flask import Flask
 
+s = os.environ.get('APP_MSG')
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return 'Jenkins Test App'
+    return f'Jenkins Test App: {s}'
 
 
 if __name__ == '__main__':
