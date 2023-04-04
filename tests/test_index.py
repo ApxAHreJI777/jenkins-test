88f7109aefd25d4280d765b6ffe5d29c9c530be0
Sergey Soltan <srg.soltan@gmail.com>
2023-04-04 18:14:35 +0200

    url

--- tests/test_index.py
+++ tests/test_index.py
@@ -9,6 +9,6 @@ class TestIndex(BaseCase):
         self.assertEqual(200, response.status_code)
 
     def test_404(self):
-        response = self.client.get('/fffffff', headers={"Content-Type": "application/json"})
+        response = self.client.get('/invalid-index-url-test-404', headers={"Content-Type": "application/json"})
 
         self.assertEqual(404, response.status_code)
